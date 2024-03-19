10f6f4d96c320d14b1206d781cbc98b9ecfaf340
csk2q <143836825+csk2q@users.noreply.github.com>
2024-03-19 15:32:37 -0500

    Setup for data

---
+++ Data/Data.cs
@@ -0,0 +1,43 @@
+namespace Ai2_Genetic_Algorithm.Data;
+
+public static class TestData
+{
+    public static readonly List<Room> Rooms = [
+        new Room("Slater 003", 45),
+        new Room("Roman 216", 30),
+        new Room("Loft 206", 75),
+        new Room("Roman 201", 50),
+        new Room("Loft 310", 108),
+        new Room("Beach 201", 60),
+        new Room("Beach 301", 75),
+        new Room("Logos 325", 450),
+        new Room("Frank 119", 60)
+    ];
+
+}
+
+public enum Facilitators
+    {
+        Lock, Glen, Banks, Richards, Shaw, Singer, Uther, Tyler, Numen, Zeldin
+    }
+
+public enum TimeSlot
+{
+    _10AM,
+    _11AM,
+    _12PM,
+    _1PM,
+    _2PM,
+    _3PM
+}
+
+public struct Room(string name, int capacity)
+{
+    public readonly string name = name;
+    public readonly int capacity = capacity;
+    public Dictionary<TimeSlot, Room> timeSlots = [];
+}
+
+
+    
+    
