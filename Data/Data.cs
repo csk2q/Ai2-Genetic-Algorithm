a7559440ffb6a78d37c4708b5a61fc1c2f34c2f4
csk2q <143836825+csk2q@users.noreply.github.com>
2024-03-20 18:51:22 -0500

    Add activities

--- Data/Data.cs
+++ Data/Data.cs
@@ -1,9 +1,13 @@
+using System.Collections.Immutable;
+
+using static Ai2_Genetic_Algorithm.Data.Facilitators;
+
 namespace Ai2_Genetic_Algorithm.Data;
 
 public static class TestData
 {
     //Make enum? name = capacity?
-    public static readonly List<Room> Rooms = [
+    public static readonly ImmutableList<Room> Rooms = [
         new Room("Slater 003", 45),
         new Room("Roman 216", 30),
         new Room("Loft 206", 75),
@@ -13,6 +17,20 @@ public static class TestData
         new Room("Beach 301", 75),
         new Room("Logos 325", 450),
         new Room("Frank 119", 60)
+    ];
+
+    public static readonly ImmutableList<Activity> Activities = [
+        new Activity("SLA100A", 50, [Glen, Lock, Banks, Zeldin], [Numen, Richards]),
+        new Activity("SLA100B", 50, [Glen, Lock, Banks, Zeldin], [Numen, Richards]),
+        new Activity("SLA191A", 50, [Glen, Lock, Banks, Zeldin], [Numen, Richards]),
+        new Activity("SLA191B", 50, [Glen, Lock, Banks, Zeldin], [Numen, Richards]),
+        new Activity("SLA201", 50, [Glen, Banks, Zeldin, Shaw], [Numen, Richards, Singer]),
+        new Activity("SLA291", 50, [Lock, Banks, Zeldin, Singer], [Numen, Richards, Shaw, Tyler]),
+        new Activity("SLA303", 60, [Glen, Zeldin, Banks], [Numen, Singer, Shaw]),
+        new Activity("SLA304", 25, [Glen, Banks, Tyler], [Numen, Singer, Shaw, Richards, Uther, Zeldin]),
+        new Activity("SLA394", 20, [Tyler, Singer], [Richards, Zeldin]),
+        new Activity("SLA449", 60, [Tyler, Singer, Shaw], [Zeldin, Uther]),
+        new Activity("SLA451", 100, [Tyler, Singer, Shaw], [Zeldin, Uther, Richards, Banks])
     ];
 
 }
